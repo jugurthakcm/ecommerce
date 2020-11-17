ce164d1aba5df82ce376bc9c341e340eafc8fa53
KACIMI Jugurtha <kjugurtha1711@gmail.com>
2020-11-17 11:03:03 +0100

    Add search bar and auth icon to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import Badge from '@material-ui/core/Badge';
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import PersonOutlineOutlinedIcon from '@material-ui/icons/PersonOutlineOutlined';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 import { useCartContext } from '../data/CartProvider';
+import SearchIcon from '@material-ui/icons/Search';
 
 const Navbar = () => {
   const items = useCartContext()[0];
@@ -13,18 +15,26 @@ const Navbar = () => {
     cart.classList.add('cart__open');
   };
   return (
-    <nav className="app__navbar">
+    <nav className="navbar">
       <h1>
         <Link to="/">Ecommerce</Link>
       </h1>
-
-      <Badge badgeContent={items.length} color="secondary">
-        <ShoppingCartOutlinedIcon
-          className="app__navbarShoppingIcon"
-          variant="contained"
-          onClick={handleCartOpen}
-        />
-      </Badge>
+      <div className="navbar__right">
+        <span className="navbar__rightSearch">
+          <SearchIcon />
+          <input placeholder="Search ..." />
+        </span>
+        <span className="navbar__rightAccount">
+          <PersonOutlineOutlinedIcon fontSize="large" />
+        </span>
+        <Badge badgeContent={items.length} color="secondary">
+          <ShoppingCartOutlinedIcon
+            className="navbar__ShoppingIcon"
+            variant="contained"
+            onClick={handleCartOpen}
+          />
+        </Badge>
+      </div>
     </nav>
   );
 };
