86f04796c97966b4e69c462000b4592e8b929dfd
KACIMI Jugurtha <kjugurtha1711@gmail.com>
2020-11-24 15:55:18 +0100

    Sticky position in items categories

--- src/components/ItemsContainer.js
+++ src/components/ItemsContainer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import './ItemsContainer.css';
 import { filterCategories } from '../util';
 import { ItemsData } from '../data/itemsData';
@@ -7,22 +7,29 @@ import Item from './Item';
 const ItemsContainer = () => {
   const itemsAPI = ItemsData();
   const categories = itemsAPI ? filterCategories(itemsAPI) : null;
-
   const [categoryItems, setCategoryItems] = useState('men clothing');
   const filtredItems =
     itemsAPI &&
     itemsAPI
       .filter((item) => item.category === categoryItems)
       .map((item) => <Item item={item} key={item.id} />);
+  const categoriesRef = useRef();
 
   const handleChangeCategory = (e) => {
     console.log(e.target.textContent);
     setCategoryItems(e.target.textContent);
   };
 
+  // window.onscroll = () => {
+  //   if (window.scrollY > categoriesRef.current.offsetTop) {
+  //     categoriesRef.current.style.position = 'sticky';
+  //     categoriesRef.current.style.top = '40px';
+  //   }
+  // };
+
   return (
     <div className="itemsContainer">
-      <div className="items__categories">
+      <div className="items__categories" ref={categoriesRef}>
         <h4>Categories</h4>
         <ul>
           {categories &&
