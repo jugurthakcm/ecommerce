d608f7e69fc82ad92081dd0f70a738f3e1368882
Jugurtha <kjugurtha1711@gmail.com>
2020-12-09 17:08:27 +0100

    Adding redux

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import CartItem from '../components/CartItem';
 import { useCartContext } from '../data/CartProvider';
-import { Link } from 'react-router-dom';
 import './Cart.css';
 import { formatPrice } from '../util';
-import ArrowBackRoundedIcon from '@material-ui/icons/ArrowBackRounded';
 import Navbar from '../components/Navbar';
 
 const Cart = () => {
