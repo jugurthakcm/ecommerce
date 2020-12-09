46343d57b48e9b8b5391e579e451f4889262f7cb
Jugurtha <kjugurtha1711@gmail.com>
2020-12-09 18:02:52 +0100

    Setting up the backend

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+const PORT = process.env.PORT || 5001;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect('mongodb://localhost:27017/ecommerce', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to the database'))
+  .catch((err) => console.log(err));
+
+app.listen(PORT, () => console.log(`Connected to ${PORT}`));
